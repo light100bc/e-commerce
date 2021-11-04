45f025b242bbb0df06de2d148d16e7fb814afd8f
Guanyu ZHANG <light100b.c@gmail.com>
2021-11-04 19:55:25 +1000

    first page

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './menu-item.style.scss'
+
+const MenuItem=({title,imgUrl,size})=>{
+    return(
+        <div className={`${size} menu-item`}>
+            <div className='background-image' 
+            style={{backgroundImage:`url(${imgUrl})`}} 
+            /> 
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle'>shop now</span>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem;
